b963b1b1129631579a1bee945ac3e55075e5f8ae
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-10-16 11:21:26 +0900

    10/16 express introduce2

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 const PORT=4000;
 const app=express();
+app.get("/",()=>console.log("some body is trying to go home"));
 const handleListening=()=>console.log(`Server listening on port http://localhost:${PORT}`);
 app.listen(PORT,handleListening);

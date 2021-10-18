42e8d96647f4713249fa0f51054c109223233df3
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-10-18 14:36:11 +0900

    10/18 express introduce4

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express"
 const PORT=4000;
 const app=express();
 const handleHome=(req,res)=>{
-    return res.send("i love you");
+    return res.send("<h1>i love you</h1>");
 }
 const handleLogin=(req,res)=>{
     return res.send("you login");

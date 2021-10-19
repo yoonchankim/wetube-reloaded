ae8e85af2db3863e9624de870964e8870d46732a
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-10-19 21:20:45 +0900

    10/19 express introduce5

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import express from "express"
 const PORT=4000;
 const app=express();
+const gossipMiddleware=(req,res,next)=>{
+    console.log(`some one is going to :${req.url}`);
+    next();
+}
 const handleHome=(req,res)=>{
-    return res.send("<h1>i love you</h1>");
+    return res.send("i love you");
 }
 const handleLogin=(req,res)=>{
     return res.send("you login");
 }
-app.get("/",handleHome);
+app.get("/",gossipMiddleware,handleHome);
 app.get("/login",handleLogin);
 
 const handleListening=()=>console.log(`Server listening on port http://localhost:${PORT}`);

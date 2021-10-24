2d6eeda88fb55a82f9311862848a1c6682482f4d
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-10-24 15:16:10 +0900

    10/24 express introduce7

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 import express from "express"
+import morgan from "morgan"
+const logger=morgan("dev");
 const PORT=4000;
 const app=express();
-const logger=(req,res,next)=>{
-    console.log(`${req.method} ${req.url}`);
-    next();
+const login=(req,res,next)=>{
+    return res.send("login");
 }
 
-const handleHome=(req,res)=>{
-    return res.send("i love you");
+const home=(req,res)=>{
+    return res.send("home");
 }
-
-app.get("/",logger,handleHome);
-
+app.use(logger);
+app.get("/",home);
+app.get("/login",login);
 const handleListening=()=>console.log(`Server listening on port http://localhost:${PORT}`);
 app.listen(PORT,handleListening);

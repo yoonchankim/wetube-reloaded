e3b52e19674cc4a4c5d21ec13a33367c18b31550
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-11-14 08:39:17 +0900

    11/14 database6

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -48,6 +48,13 @@ export const upload=(req,res)=>{
   return res.render("upload",{pageTitle:"Upload Video"});
 };
 export const postUpload=(req,res)=>{
+  const newVideo={title:req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length+1};
+    videos.push(newVideo);
   return res.redirect("/");
 };
 export const deleteVideo=(req,res)=>{

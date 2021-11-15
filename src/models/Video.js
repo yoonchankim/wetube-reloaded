36bbf57e85494f5dc783e64f37c47f3210db5909
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-11-16 02:25:13 +0900

    11/15 database9

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,1 +1,13 @@
-//need to decribe our db's form to mongoose
+import mongoose from "mongoose"
+const videoSchema=new mongoose.Schema({
+    title:String,
+    description:String,
+    createdAt:Date,
+    hashtags:[{type:String}],
+    meta:{
+        views:Number,
+        rating:Number
+    }
+});
+const Video=mongoose.model("Video",videoSchema);
+export default Video;

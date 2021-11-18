85b25b880bf05ca018a3db8f372e7ae249f44f17
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-11-18 19:42:42 +0900

    11/18 database12

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,7 @@
 import Video from "../models/Video"
-export const trending = (req, res) =>{
-    Video.find({},(error,videos)=>{
-      return res.render("home",{pageTitle : "Home",videos:[]});
-    });
+export const trending = async(req, res) =>{
+  const videos=await Video.find({}); 
+  return res.render("home",{pageTitle : "Home",videos:[]});
 } 
 export const watch=(req,res)=>{
   const id=req.params.id;

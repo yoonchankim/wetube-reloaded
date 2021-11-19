f95590beb341d14a8970381e6bee8aaf244b6ced
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-11-20 00:14:32 +0900

    11/19 database15

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,12 @@
 import Video from "../models/Video"
 export const trending = async(req, res) =>{
   const videos=await Video.find({}); 
-  console.log(videos);
   return res.render("home",{pageTitle : "Home",videos});
 } 
-export const watch=(req,res)=>{
+export const watch=async(req,res)=>{
   const id=req.params.id;
-  return res.render("watch",{pageTitle : `watching`});
+  const video=await Video.findById(id);
+  return res.render("watch",{pageTitle :video.title,video});
 } 
 export const getEdit=(req,res)=>{
   const id=req.params.id;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,8 +1,8 @@
 import express from "express"
 import {postUpload, watch, getEdit, upload, deleteVideo,postEdit} from "../controllers/videoController"
 const videoRouter=express.Router();
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
 videoRouter.route("/upload").get(upload).post(postUpload);
 export default videoRouter;

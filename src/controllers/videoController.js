a0ba8bceaa2cfe3304aceab6325a506f1feaabfd
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-11-20 16:19:00 +0900

    11/20 database16

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,15 +6,31 @@ export const trending = async(req, res) =>{
 export const watch=async(req,res)=>{
   const id=req.params.id;
   const video=await Video.findById(id);
-  return res.render("watch",{pageTitle :video.title,video});
+  if(video===null){
+    return res.render("404",{pageTitle:"Video Not Found"});
+  }
+  return res.render("watch",{pageTitle:video.title,video});
 } 
-export const getEdit=(req,res)=>{
+export const getEdit=async(req,res)=>{
   const id=req.params.id;
-  return res.render("edit",{pageTitle:`Editing`})
+  const video=await Video.findById(id);
+  if(video===null){
+    return res.render("404",{pageTitle:"Video Not Found"});
+  }
+  return res.render("edit",{pageTitle:`Edit "${video.title}"`,video})
 };
-export const postEdit=(req,res)=>{
+export const postEdit=async(req,res)=>{
   const id=req.params.id;
   const title=req.body.title;
+  const description=req.body.description;
+  const hashtags=req.body.hashtags;
+  const video=await Video.exist({_id:id});
+  if(!video){
+    return res.render("404",{pageTitle:"Video Not Found"});
+  }
+  await Video.findByIdAndUpdate(id,{
+    title,description,hashtags:hashtags.split(",").map((word)=>(word.startsWith(`#`)?word:`#${word}`))
+  })
   return res.redirect(`/videos/${id}`);
 };
 export const search=(req,res)=>res.send("Search");
@@ -28,7 +44,7 @@ export const postUpload=async(req,res)=>{
   try{await Video.create({
     title,
     description,
-    hashtags:hashtags.split(",").map(word=>`#${word}`),
+    hashtags:hashtags.split(",").map((word)=>(word.startsWith(`#`)?word:`#${word}`)),
   });}
   catch(error){
     return res.render("upload",{pageTitle:"Upload Video",errorMessage:error._message});

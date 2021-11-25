990dc9c3abe642dcb3e80947fac87076d23e703c
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-11-26 02:13:40 +0900

    11/25 user5

---
+++ src/middlewares.js
@@ -0,0 +1,7 @@
+export const localMiddleware=(req,res,next)=>{
+    res.locals.loggedIn=Boolean(req.session.loggedIn);
+    res.locals.siteName="wetube"
+    res.locals.loggedInUsers=req.session.user;
+    console.log(res.locals);
+    next();
+}
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express"
 import morgan from "morgan"
 import session from "express-session"
+import MongoStore from "connect-mongo"
 import rootRouter from "./routers/rootRouter"
 import videoRouter from "./routers/videoRouter"
 import userRouter from "./routers/userRouter"
+import {localMiddleware} from "./middlewares"
 const app=express();
 const logger=morgan("dev");
 app.set("view engine","pug");
@@ -11,15 +13,9 @@ app.set("views",process.cwd()+"/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
 app.use(session({
-    secret:"Hello!",resave:true,saveUninitialized:true
+    secret:"Hello!",resave:false,saveUninitialized:false,store:MongoStore.create({mongoUrl:"mongodb://127.0.0.1:27017/wetube"})
 }))
-app.use((req, res, next) => {
-    req.sessionStore.all((error, sessions) => {
-      console.log(sessions);
-      next();
-    });
-  });
-  
+app.use(localMiddleware);
 app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
